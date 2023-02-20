5530504daf74d9fb4b9f127e02e89cbe183faae9
fedeloxe <loxeee@gmail.com>
2023-02-20 19:34:02 +0100

    push stampa card esercizio giovedì 16

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,3 @@
+<header>
+    <a href="{{ route('comics')}}">Comics</a>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/comics', function () {
+    $comics = config('db.comics');
+
+    return view('comics', compact('comics'));
+})->name('comics');
